c06bf13a28d291bd4785510639d4b1dd112fdc51
江鹏利 <845415120@qq.com>
2024-01-02 18:53:36 +0800

    first

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // 配置浏览器标题
+  title: '人力资源后台系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  // 固定首页头部
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  // logo 展示
+  sidebarLogo: true
+}
